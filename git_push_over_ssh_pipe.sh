875c75512d43d66786576b023a772dca4e3f37fd
Zenon Mousmoulas <zmousm@noc.grnet.gr>
2014-10-17 02:42:09 +0300

    second reincarnation

--- git_push_over_ssh_pipe.sh
+++ git_push_over_ssh_pipe.sh
@@ -1,6 +1,29 @@
-ssh dst-host \
-  "git receive-pack /path/to/receiving-repo" \
- </tmp/justafifo | \
-ssh src-host \
-  "cd /path/to/source-repo; socat - EXEC:'git send-pack --all --receive-pack=\\\"socat - 5 #\\\" .',fdin=5" \
- >/tmp/justafifo
+#!/bin/bash
+
+function git-pipe-push () {
+    local shost spath dhost dpath
+    eval $(perl -e \
+'my @d = qw(s d);
+for (my $i=0; $i<=1; $i++) {
+  if ($ARGV[$i] =~ /^(\[[0-9A-Fa-f:]+\]|[^:]+):(.*)$/) {
+    printf "%1\$shost=\"%2\$s\" %1\$spath=\"%3\$s\" ", $d[$i], $1, $2;
+  }
+}' "${@:1:2}")
+    if [ -z "$shost" -o -z "$dhost" -o -z "$spath" -o -z "$dpath" ]; then
+	return 1
+    else
+	shift 2
+    fi
+    justafifo=$(mktemp -u /tmp/gitpushpipe.XXXXXX)
+    mkfifo "$justafifo"
+    trap 'rm -f "$justafifo"' HUP INT QUIT TERM KILL
+    ssh $shost \
+	"cd $spath; socat - EXEC:'git send-pack --receive-pack=\\\"socat - 5 #\\\" /dev/null ${@//:/\:}',fdin=5" \
+      <"$justafifo" | \
+    ssh $dhost \
+	"git receive-pack $dpath" \
+      >"$justafifo"
+    rc=$?
+    rm -f "$justafifo"
+    return $rc
+}
