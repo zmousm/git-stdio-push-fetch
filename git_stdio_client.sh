ac016affccfa09cbdc8eb30fda74694d3a5a63d5
Zenon Mousmoulas <zmousm@noc.grnet.gr>
2014-10-21 15:10:50 +0300

    Add option to run commands with sudo, only possible with in the functions using the git commands for now.

--- git_stdio_client.sh
+++ git_stdio_client.sh
@@ -115,7 +115,7 @@ for (my $i=0; $i<=1; $i++) {
 }
 
 function git-pipe-push2 () {
-    local shost spath dhost dpath justafifo rc
+    local shost spath dhost dpath opts=() sudo justafifo rc
     if [ $# -ge 2 ]; then
 	eval $(perl -e \
 'my @d = qw(s d);
@@ -126,11 +126,26 @@ for (my $i=0; $i<=1; $i++) {
 }' "${@:1:2}")
     fi
     if [ -z "$shost" -o -z "$dhost" -o "$1" = "-h" ]; then
-	echo "Usage: ${FUNCNAME[0]} sending-host:[path] receiving-host:[path] [git-send-pack options]" >&2
+	echo "Usage: ${FUNCNAME[0]} sending-host:[path] receiving-host:[path] [--sudo[=user]] [git-send-pack options]" >&2
 	[ "$1" = "-h" ] && return 0 || return 1
     else
 	shift 2
     fi
+    while [ -n "$1" ]; do
+	case "$1" in
+	    --sudo*)
+		if [ -z "${1#--sudo}" ]; then
+		    sudo=root
+		else
+		    sudo="${1#--sudo=}"
+		fi
+		;;
+	    *)
+		opts+=("$1")
+		;;
+	esac
+	shift
+    done
     justafifo=$(mktemp -u /tmp/gitpipe.XXXXXX)
     mkfifo "$justafifo"
     rc=$?
@@ -139,20 +154,34 @@ for (my $i=0; $i<=1; $i++) {
     else
 	return $rc
     fi
-    ssh $shost \
-	"cd \"$spath\";"\
-	"git pipe-send-pack $@;"\
-      <"$justafifo" | \
-    ssh $dhost \
-	"git receive-pack \"$dpath\"" \
-      >"$justafifo"
+    if [ -n "$sudo" ]; then
+	ssh $shost \
+	    "sudo -u \"$sudo\" -- sh -c '"\
+	    "cd \"$spath\";"\
+	    "git pipe-send-pack ${opts[@]};"\
+	    "'"\
+	  <"$justafifo" | \
+	ssh $dhost \
+	    "sudo -u \"$sudo\" -- sh -c '"\
+	    "git receive-pack \"$dpath\"" \
+	    "'"\
+	  >"$justafifo"
+    else
+	ssh $shost \
+	    "cd \"$spath\";"\
+	    "git pipe-send-pack ${opts[@]};"\
+	  <"$justafifo" | \
+	ssh $dhost \
+	    "git receive-pack \"$dpath\"" \
+	  >"$justafifo"
+    fi
     rc=$?
     rm -f "$justafifo"
     return $rc
 }
 
 function git-pipe-fetch2 () {
-    local shost spath dhost dpath remote justafifo rc
+    local shost spath dhost dpath opts=() remote sudo justafifo rc
     if [ $# -ge 2 ]; then
 	eval $(perl -e \
 'my @d = qw(s d);
@@ -163,15 +192,34 @@ for (my $i=0; $i<=1; $i++) {
 }' "${@:1:2}")
     fi
     if [ -z "$shost" -o -z "$dhost" -o "$1" = "-h" ]; then
-	echo "Usage: ${FUNCNAME[0]} receiving-host:[path] sending-host:[path] [--remote=remote_name] [git-fetch-pack options]" >&2
+	echo "Usage: ${FUNCNAME[0]} receiving-host:[path] sending-host:[path] [--remote=remote_name] [--sudo[=user]] [git-fetch-pack options]" >&2
 	[ "$1" = "-h" ] && return 0 || return 1
     else
 	shift 2
     fi
-    if test -n "$1" && echo "$1" | grep -q "^--remote="; then
-	remote="${1#--remote=}"
+    while [ -n "$1" ]; do
+	case "$1" in
+	    --remote*)
+		if [ -z "${1#--remote}" ]; then
+		    echo "missing required argument to --remote" >&2
+		    return 1
+		else
+		    remote="${1#--remote=}"
+		fi
+		;;
+	    --sudo*)
+		if [ -z "${1#--sudo}" ]; then
+		    sudo=root
+		else
+		    sudo="${1#--sudo=}"
+		fi
+		;;
+	    *)
+		opts+=("$1")
+		;;
+	esac
 	shift
-    fi
+    done
     justafifo=$(mktemp -u /tmp/gitpipe.XXXXXX)
     mkfifo "$justafifo"
     rc=$?
@@ -180,16 +228,33 @@ for (my $i=0; $i<=1; $i++) {
     else
 	return $rc
     fi
-    ssh $shost \
-	"cd \"$spath\";"\
-	"export refstmp=$(mktemp -u /tmp/gitfetchpack.XXXXXXXX);"\
-	"git pipe-fetch-pack --write-refs=\${refstmp} $@;"\
-	"gfp2gur.awk -v remote=\"${remote:-$dhost}\" \${refstmp} | xargs -L 1 git;"\
-	"rm \${refstmp}"\
-      <"$justafifo" | \
-    ssh $dhost \
-	"git upload-pack \"$dpath\"" \
-      >"$justafifo"
+    if [ -n "$sudo" ]; then
+	ssh $shost \
+	    "sudo -u \"$sudo\" -- sh -c '"\
+	    "cd \"$spath\";"\
+	    "export refstmp=$(mktemp -u /tmp/gitfetchpack.XXXXXXXX);"\
+	    "git pipe-fetch-pack --write-refs=\${refstmp} ${opts[@]};"\
+	    "gfp2gur.awk -v remote=\"${remote:-$dhost}\" \${refstmp} | xargs -L 1 git;"\
+	    "rm \${refstmp}"\
+	    "'"\
+	  <"$justafifo" | \
+	ssh $dhost \
+	    "sudo -u \"$sudo\" -- sh -c '"\
+	    "git upload-pack \"$dpath\"" \
+	    "'"\
+	  >"$justafifo"
+    else
+	ssh $shost \
+	    "cd \"$spath\";"\
+	    "export refstmp=$(mktemp -u /tmp/gitfetchpack.XXXXXXXX);"\
+	    "git pipe-fetch-pack --write-refs=\${refstmp} ${opts[@]};"\
+	    "gfp2gur.awk -v remote=\"${remote:-$dhost}\" \${refstmp} | xargs -L 1 git;"\
+	    "rm \${refstmp}"\
+	  <"$justafifo" | \
+	ssh $dhost \
+	    "git upload-pack \"$dpath\"" \
+	  >"$justafifo"
+    fi
     rc=$?
     rm -f "$justafifo"
     return $rc
